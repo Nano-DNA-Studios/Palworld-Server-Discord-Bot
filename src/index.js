24bd90f1345c0e79aba42af4e336819cec470fed
Alexandre Dufresne Nappert <93613553+MrDNAlex@users.noreply.github.com>
2024-01-28 12:06:33 -0500

    Token Save

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-const { Client, IntentsBitField} = require("discord.js");
+require('dotenv').config();
+const {Client, IntentsBitField} = require('discord.js');
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ]
+
+    intents: [IntentsBitField.Flags.Guilds,
+     IntentsBitField.Flags.GuildMembers,
+     IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+     IntentsBitField.Flags.MessageContent,]
 });
+
+client.on('ready', (c) => {
+    console.log(`Bot is ready ${c.user.tag}`);
+});
+
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return;
+
+    console.log(message.content);
+     if(message.content === 'ping'){
+         message.reply('pong');
+     }
+});
+
+
+
+client.login('MTIwMTE5ODM0NDE5ODk2NzMxNg.GMClIp.Ja0IyaVEiF7V9JdD2IjBEeXyBtiswM5PPBHIlY');
