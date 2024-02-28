9a67d3e5a6ba02117388958360b4d122b7f6c087
Alexandre Dufresne Nappert <93613553+MrDNAlex@users.noreply.github.com>
2024-02-28 00:38:06 -0500

    Revert "Successful build"
    This reverts commit ff5f5c69af4fc648656151adeebc95fdb029c124.

--- src/EmptyCustomCommandHandler.js
+++ src/EmptyCustomCommandHandler.js
@@ -1,10 +1,4 @@
 "use strict";
-/**
- * Empty Command Handler
- * @param dataManager Bot Data Manager
- * @param interaction Command Interaction
- * @param client Discord Bot Client
- */
 function EmptyCustomCommandHandler(dataManager, interaction, client) {
     return new Promise((resolve, reject) => {
         resolve();
