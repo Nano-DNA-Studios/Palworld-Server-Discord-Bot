369b8dc6bf975ea9277de9eebafb46e90173f8ff
Alexandre Dufresne Nappert <93613553+MrDNAlex@users.noreply.github.com>
2024-03-01 20:45:23 -0500

    Managed to Load Default Commands
    - Loaded default commands
    - Tested out new register method
    - Linked to local package

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const FileSearch_1 = __importDefault(require("dna-discord-framework/src/FileSearch"));
 const PalworldBotDataManager_1 = __importDefault(require("./PalworldBotDataManager"));
 const DiscordBot_1 = __importDefault(require("dna-discord-framework/src/Bot/DiscordBot"));
 let Bot = new DiscordBot_1.default(PalworldBotDataManager_1.default);
 Bot.StartBot();
-console.log(new FileSearch_1.default().GetAllJSFiles());
+//console.log(new FileSearch().GetAllJSFiles())
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-import FileSearch from "dna-discord-framework/src/FileSearch";
 import PalworldBotDataManager from "./PalworldBotDataManager";
 import DiscordBot from "dna-discord-framework/src/Bot/DiscordBot";
-
 
 let Bot = new DiscordBot(PalworldBotDataManager);
 
 Bot.StartBot();
-
-console.log(new FileSearch().GetAllJSFiles())
