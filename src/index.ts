951420c6dffd97a15aed66e7b3cb48cc062d5031
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-03-01 19:19:26 -0500

    Console Log

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import FileSearch from "dna-discord-framework/src/FileSearch";
 import PalworldBotDataManager from "./PalworldBotDataManager";
 import DiscordBot from "dna-discord-framework/src/Bot/DiscordBot";
 
@@ -5,3 +6,5 @@ import DiscordBot from "dna-discord-framework/src/Bot/DiscordBot";
 let Bot = new DiscordBot(PalworldBotDataManager);
 
 Bot.StartBot();
+
+console.log(new FileSearch().GetAllJSFiles())
